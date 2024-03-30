9418aa2739f20254528b98ee62d0a0d3d895b52a
maayannachmias <maayansharvit1997@gmail.com>
2024-03-30 19:22:47 +0300

    change name of button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -106,7 +106,7 @@ const Login = () => {
                                     type="submit"
                                     className="group relative flex w-full justify-center rounded-md border border-transparent bg-purple-700 py-2 px-4 text-sm font-medium text-white hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 >
-                                    Sign in
+                                    Log in
                                 </button>
                                  {error && <div className="error">{error}</div>}
 
